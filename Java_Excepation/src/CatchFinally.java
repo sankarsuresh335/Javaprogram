d169d6f8c7a9be1de9529358b3cf2338f8b43cd1
admin <admin@DESKTOP-2DR0QON>
2019-09-19 22:35:18 -0500

    Added new file java Catch file

---
+++ Java_Excepation/src/CatchFinally.java
@@ -0,0 +1,36 @@
+
+public class CatchFinally {
+
+	
+	int i =10;
+	
+	public void mymethod() {
+		
+		try {
+			
+			int a=i/0;	
+			
+			System.out.println(a);
+			
+		}catch(Exception e){
+			System.out.println("i am inside the cath");
+			e.printStackTrace();
+		}
+				
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	CatchFinally cathch= new CatchFinally();
+	
+	cathch.mymethod();
+	
+	
+	
+	
+	
+		
+		
+	}
+
+}
